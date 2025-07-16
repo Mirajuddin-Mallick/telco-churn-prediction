8eb5575370e03c3bf6071a235ee5c6b9f5c24894
Mirajuddin-Mallick <mirajmallick550@gmail.com>
2025-07-16 16:22:55 +0530

    Update app.py

--- telco-churn-prediction/app.py
+++ telco-churn-prediction/app.py
@@ -65,9 +65,7 @@ def analysis():
 def about():
     return render_template('about.html')
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
